2650d27fb0b69bfd854ed448dc874bf4c6e62f90
Gregor Jasny <gregor.jasny@logmein.com>
2021-07-01 14:16:42 +0200

    Add corpus

--- fuzz.cpp
+++ fuzz.cpp
@@ -4,8 +4,9 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   try {
     Url url{std::string{reinterpret_cast<const char*>(data), size}};
+    (void)url.port();
   } catch (Url::parse_error&) {
-      
+
   }
   return 0;
 }
